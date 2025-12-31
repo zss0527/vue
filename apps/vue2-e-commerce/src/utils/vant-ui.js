d0e8fccf2c27c11f2d0d6231311c58c040a634b3
zss0527 <zss0527@outlook.com>
2025-12-31 15:56:25 +0800

    登录页，axios创建实例并配置该实例,图形验证码功能实现

--- apps/vue2-e-commerce/src/utils/vant-ui.js
+++ apps/vue2-e-commerce/src/utils/vant-ui.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import { Button, Tabbar, TabbarItem } from 'vant'
+import { Button, Tabbar, TabbarItem, NavBar } from 'vant'
 
+Vue.use(NavBar)
 Vue.use(Button)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
