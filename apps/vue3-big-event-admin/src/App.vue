3331f55df02d0cbc5ba9b0a1d81b6d016716c306
zss0527 <zss0527@outlook.com>
2026-01-04 17:21:52 +0800

    element-plus, auto-importer plugin

--- apps/vue3-big-event-admin/src/App.vue
+++ apps/vue3-big-event-admin/src/App.vue
@@ -1,9 +1,22 @@
 <script setup>
+import {useRouter} from "vue-router";
+import {ref} from "vue";
+
+const router = useRouter()
+const goList = () => {
+  router.push("/list")
+}
+const checked1 = ref(false)
+const checked2 = ref(true)
 </script>
 
 <template>
   <div>
     i am app
+    <el-button @click="$router.push('/home')">home</el-button>
+    <el-button @click="goList">list</el-button>
+    <el-checkbox v-model="checked1" disabled>Disabled</el-checkbox>
+    <el-checkbox v-model="checked2">Not disabled</el-checkbox>
   </div>
 </template>
 
